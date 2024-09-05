642e0b3defcc644c2a5e6ca98ee43ac5bff87569
Anton Bergström <60569011+aber007@users.noreply.github.com>
2024-09-05 20:20:28 +0200

    Added hashmaps

--- TryingToConvertMazeAlgorithm/src/Main.java
+++ TryingToConvertMazeAlgorithm/src/Main.java
@@ -7,18 +7,24 @@ public class Main {
         int xcord = 5;
         int ycord = 5;
         HashMap<String, String> grids = new HashMap<>();
+        HashMap<String, String> gridwalls = new HashMap<>();
         Scanner input = new Scanner(System.in);
         for (int i = 0; i<xcord; i++){
-            String xcordFormatted = String.format("%03d", i);
             for (int j = 0; j<ycord; j++){
+                String xcordFormatted = String.format("%03d", i);
                 String ycordFormatted = String.format("%03d", j);
-                grids.put(xcordFormatted+","+ycordFormatted, null);
-
+                grids.put(xcordFormatted+","+ycordFormatted, "grid");
+                gridwalls.put(xcordFormatted+","+ycordFormatted + ",y", "ywall");
+                gridwalls.put(xcordFormatted+","+ycordFormatted + ",x", "xwall");
             }
         }
+
         System.out.println(grids);
         System.out.println("Choose a grid");
         String grid = input.nextLine();
         System.out.println(grids.get(grid));
+        System.out.println(gridwalls.get(grid+",y"));
+        System.out.println(gridwalls.get(grid+",x"));
+
     }
 }
