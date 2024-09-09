deaab3ea3f540173b51d62db0689e0c1e7a8ea51
Anton Bergström <60569011+aber007@users.noreply.github.com>
2024-09-09 14:54:16 +0200

    ...

--- TryingToConvertMazeAlgorithm/src/Main.java
+++ TryingToConvertMazeAlgorithm/src/Main.java
@@ -9,18 +9,18 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //Create a grid
-        int xcord = 150;
-        int ycord = 150;
+        int xcord = 10;
+        int ycord = 10;
         HashMap<String, JPanel> grids = new HashMap<>();
-        HashMap<String, String> gridwalls = new HashMap<>();
+        HashMap<String, JPanel> gridwalls = new HashMap<>();
         Scanner input = new Scanner(System.in);
         for (int i = 0; i<xcord; i++){
             for (int j = 0; j<ycord; j++){
                 String xcordFormatted = String.format("%03d", i);
                 String ycordFormatted = String.format("%03d", j);
                 grids.put(xcordFormatted+","+ycordFormatted, null);
-                gridwalls.put(xcordFormatted+","+ycordFormatted + ",y", "false");
-                gridwalls.put(xcordFormatted+","+ycordFormatted + ",x", "false");
+                gridwalls.put(xcordFormatted+","+ycordFormatted + ",y", null);
+                gridwalls.put(xcordFormatted+","+ycordFormatted + ",x", null);
             }
         }
         //create main window
@@ -36,7 +36,7 @@ public class Main {
         gridPanel.setBounds(0, 0, 780, 780);
         gridPanel.setBackground(Color.WHITE);
         System.out.println("gridsize:" + xcord +","+ ycord);
-        gridPanel.setLayout(new GridLayout(xcord, ycord, 1, 1));  // 5x5 grid with 2px gaps
+        gridPanel.setLayout(new GridLayout(xcord, ycord, 0, 0));  // 5x5 grid with 2px gaps
 
         mainFrame.add(gridPanel);
         mainFrame.setVisible(true);
@@ -48,19 +48,27 @@ public class Main {
 
                 // Create small panels to fill the grid
                 JPanel smallGridPanel = new JPanel();
-                smallGridPanel.setBackground(Color.GRAY);  // Set the background color to gray
+                smallGridPanel.setBackground(Color.BLACK);  // Set the background color to gray
 
                 // Store the JPanel in the HashMap with the key
                 grids.put(key, smallGridPanel);
 
                 // Add the panel to the gridPanel (which has GridLayout)
                 gridPanel.add(smallGridPanel);
+
+                // Add Horizontal and Vertical Walls to each smallGridPanel
+                int smallGridSizeWall = smallGridPanel.getPreferredSize().width;
+                JPanel GridWallVertical = new JPanel();
+                GridWallVertical.setBackground(Color.GRAY);
+                gridwalls.put((key+"x"),GridWallVertical);
+                
+                smallGridPanel.add(GridWallVertical);
+
+
             }
         }
         //check if all grids exist
         System.out.println(grids.size());
-
-        System.out.println(grids);
         JPanel targetPanel = grids.get("000,000");
         System.out.println(targetPanel);
         targetPanel.setBackground(Color.BLUE);
@@ -167,7 +175,6 @@ public class Main {
         //removeWallIfGoingOver();
         //checkIfNearbyWallsExists();
         //checkIfNearbyGridsHasAllWalls();
-
 
     }
     public static void updateMarker(String lastLocationKey, String currentLocationKey, HashMap<String, JPanel> grids) {
