2d5bc390fe38b1c3d4e3b771d441eee637e85ed4
Anton Bergström <60569011+aber007@users.noreply.github.com>
2024-09-09 14:05:09 +0200

    Can create grids and movement. no walls

--- TryingToConvertMazeAlgorithm/src/Main.java
+++ TryingToConvertMazeAlgorithm/src/Main.java
@@ -1,14 +1,16 @@
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         //Create a grid
-        int xcord = 5;
-        int ycord = 5;
+        int xcord = 150;
+        int ycord = 150;
         HashMap<String, JPanel> grids = new HashMap<>();
         HashMap<String, String> gridwalls = new HashMap<>();
         Scanner input = new Scanner(System.in);
@@ -31,8 +33,9 @@ public class Main {
 
         // Create gridPanel with GridLayout to automatically manage the grid
         JPanel gridPanel = new JPanel();
-        gridPanel.setBounds(0, 0, 700, 700);
+        gridPanel.setBounds(0, 0, 780, 780);
         gridPanel.setBackground(Color.WHITE);
+        System.out.println("gridsize:" + xcord +","+ ycord);
         gridPanel.setLayout(new GridLayout(xcord, ycord, 1, 1));  // 5x5 grid with 2px gaps
 
         mainFrame.add(gridPanel);
@@ -54,15 +57,124 @@ public class Main {
                 gridPanel.add(smallGridPanel);
             }
         }
+        //check if all grids exist
+        System.out.println(grids.size());
+
         System.out.println(grids);
         JPanel targetPanel = grids.get("000,000");
         System.out.println(targetPanel);
         targetPanel.setBackground(Color.BLUE);
 
         int[] currentLocation = {0,0};
+        mainFrame.addKeyListener(new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+                switch (keyCode) {
+                    case KeyEvent.VK_UP:
+                        moveNorth(currentLocation, grids);
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        moveSouth(currentLocation, ycord, grids);
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        moveWest(currentLocation, grids);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        moveEast(currentLocation, xcord, grids);
+                        break;
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+        });
+
+        // Make sure the JFrame is focusable for key events
+        mainFrame.setFocusable(true);
+        mainFrame.requestFocusInWindow();
+    }
+
+    public static void moveEast(int[] currentLocation, int xcord ,HashMap<String, JPanel> grids) {
+        System.out.println("E");
+        int[] lastLocation = Arrays.copyOf(currentLocation, currentLocation.length);
+        if (currentLocation[1] < (xcord-1)) {
+            currentLocation[1]++;
+        }
+        String xkey = String.format("%03d", currentLocation[0]);
+        String ykey = String.format("%03d", currentLocation[1]);
+        String currentLocationKey = xkey + "," + ykey;
+        update(lastLocation,currentLocation, grids);
+
+
 
     }
-    public void moveRight(String currentLocation){
 
+    public static void moveWest(int[] currentLocation, HashMap<String, JPanel> grids){
+        System.out.println("W");
+        int[] lastLocation = Arrays.copyOf(currentLocation, currentLocation.length);
+        if (currentLocation[1] > 0){
+            currentLocation[1]--;
+        }
+        String xkey = String.format("%03d", currentLocation[0]);
+        String ykey = String.format("%03d", currentLocation[1]);
+        String currentLocationKey = xkey + "," + ykey;
+        update(lastLocation,currentLocation, grids);
+
+
+
+    }
+
+    public static void moveNorth(int[] currentLocation, HashMap<String, JPanel> grids){
+        System.out.println("N");
+        int[] lastLocation = Arrays.copyOf(currentLocation, currentLocation.length);
+        if (currentLocation[0] > 0){
+            currentLocation[0]--;
+        }
+        String xkey = String.format("%03d", currentLocation[0]);
+        String ykey = String.format("%03d", currentLocation[1]);
+        String currentLocationKey = xkey + "," + ykey;
+        update(lastLocation,currentLocation, grids);
+
+    }
+
+    public static void moveSouth(int[] currentLocation, int ycord, HashMap<String, JPanel> grids){
+        System.out.println("S");
+        int[] lastLocation = Arrays.copyOf(currentLocation, currentLocation.length);
+        if (currentLocation[0] < (ycord-1)){
+            currentLocation[0]++;
+        }
+        String xkey = String.format("%03d", currentLocation[0]);
+        String ykey = String.format("%03d", currentLocation[1]);
+        String currentLocationKey = xkey + "," + ykey;
+        update(lastLocation,currentLocation, grids);
+
+    }
+    public static void update(int[] lastLocation, int[] currentLocation, HashMap<String, JPanel> grids) {
+        // Initial variable creation
+        String currentLocationKey = String.format("%03d", currentLocation[0]) + "," + String.format("%03d", currentLocation[1]);
+        String lastLocationKey = String.format("%03d", lastLocation[0]) + "," + String.format("%03d", lastLocation[1]);
+        System.out.println("IN UPDATE: " + lastLocationKey + "," + currentLocationKey);
+
+        //functions
+        updateMarker(lastLocationKey, currentLocationKey, grids);
+
+        //removeWallIfGoingOver();
+        //checkIfNearbyWallsExists();
+        //checkIfNearbyGridsHasAllWalls();
+
+
+    }
+    public static void updateMarker(String lastLocationKey, String currentLocationKey, HashMap<String, JPanel> grids) {
+        JPanel oldTargetPanel = grids.get(lastLocationKey);
+        oldTargetPanel.setBackground(Color.GRAY);
+        JPanel targetPanel = grids.get(currentLocationKey);
+        targetPanel.setBackground(Color.BLUE);
     }
 }
+
